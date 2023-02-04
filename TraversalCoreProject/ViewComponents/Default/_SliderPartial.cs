0c905993a58379def0c33e80b2cbad0134d27a78
Umut Tosun <tosun.umutt@gmail.com>
2023-02-04 17:30:49 +0300

    SliderPartial created.

---
+++ TraversalCoreProject/ViewComponents/Default/_SliderPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProject.ViewComponents.Default
+{
+    public class _SliderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
