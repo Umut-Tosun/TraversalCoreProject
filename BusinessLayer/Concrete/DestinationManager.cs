32054afd03241cd3a76e516989b854f22b3d737d
Umut Tosun <tosun.umutt@gmail.com>
2023-02-05 20:06:47 +0300

    Added new managers.

---
+++ BusinessLayer/Concrete/DestinationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class DestinationManager : IDestinationService
+    {
+        IDestinationDal _destinationDal;
+
+        public DestinationManager(IDestinationDal destinationDal)
+        {
+            _destinationDal = destinationDal;
+        }
+
+        public void TAdd(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Destination TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Destination> TGetList()
+        {
+            return _destinationDal.GetList();
+        }
+
+        public void TUpdate(Destination t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessLayer/Concrete/SubAboutManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SubAboutManager : ISubAboutService
+    {
+        ISubAboutDal subAboutDal;
+
+        public SubAboutManager(ISubAboutDal subAboutDal)
+        {
+            this.subAboutDal = subAboutDal;
+        }
+
+        public void TAdd(SubAbout t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(SubAbout t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SubAbout TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SubAbout> TGetList()
+        {
+            return subAboutDal.GetList();
+        }
+
+        public void TUpdate(SubAbout t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
