f184cfe9b23a26b2377752582804a77e710595c3
Umut Tosun <tosun.umutt@gmail.com>
2023-02-17 20:51:59 +0300

    Update generic dal and comment dal.

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,7 @@ namespace DataAccessLayer.Abstract
         void Delete(T t);
         void Update(T t);
         List<T> GetList();
+        T GetById(int id);
+        List<T> GetListByFilter(Expression<Func<T,bool>> filter);
     }
 }
