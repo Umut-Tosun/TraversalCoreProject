34402e8d7d873a64a819d5c3c1228fd1bef35879
Umut Tosun <tosun.umutt@gmail.com>
2023-02-17 21:44:50 +0300

    Add login controller and views for login.

---
+++ TraversalCoreProject/Controllers/LoginController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TraversalCoreProject.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public IActionResult SignUp()
+        {
+            return View();
+        }*/
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public IActionResult SignIn()
+        {
+            return View();
+        }*/
+    }
+}
