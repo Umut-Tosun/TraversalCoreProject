07fead61a9fd2ab008a05dc08c55902e6ba7263e
Umut Tosun <tosun.umutt@gmail.com>
2023-02-18 18:32:26 +0300

    Add new register theme.

---
+++ TraversalCoreProject/wwwroot/colorlib-regform-25/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+	var dp1 = $('#dp1').datepicker().data('datepicker');
+	dp1.selectDate(new Date());
+	var dp2 = $('#dp2').datepicker().data('datepicker');
+	dp2.selectDate(new Date());
+})
