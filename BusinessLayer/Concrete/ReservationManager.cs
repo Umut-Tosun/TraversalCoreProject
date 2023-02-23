857381cfefeac063588ab403fb209861bc235b5d
Umut Tosun <tosun.umutt@gmail.com>
2023-02-23 22:02:54 +0300

    Add services and manager for entity reservation.

---
+++ BusinessLayer/Concrete/ReservationManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class ReservationManager : IReservationService
+    {
+        IReservationDal _reservationDal;
+
+        public ReservationManager(IReservationDal reservationDal)
+        {
+            _reservationDal = reservationDal;
+        }
+
+        public void TAdd(Reservation t)
+        {
+            _reservationDal.Insert(t);
+        }
+
+        public void TDelete(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Reservation TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Reservation> TGetList()
+        {
+          return _reservationDal.GetList();
+        }
+
+        public void TUpdate(Reservation t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
